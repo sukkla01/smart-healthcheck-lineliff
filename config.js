9ba37c1d6b09e75ea0e6c393818534531aec5629
sukkla01 <sukkla01@gmail.com>
2023-01-05 16:51:17 +0700

    config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     font: 'Kanit',
-    // BASE_URL: 'https://api-queue-ss.diligentsoftinter.com',
-    BASE_URL: 'http://localhost:4012',
+    BASE_URL: 'https://api-smart-healthcheck.diligentsoftinter.com',
+    // BASE_URL: 'http://localhost:4012',
     token : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImljdDAwOSIsImRlcF9wYXRoIjoiMTA3MjUiLCJ0bmFtZSI6IuC4quC4uOC4iOC4tOC4meC4leC5jCDguKrguLjguIHguIHguKXguYnguLIiLCJjaWQiOiIxNjQwNzAwMDE1NDYxIiwicGxhdGZvcm0iOiJ3ZWJfc3dob3NwaXRhbCIsInJvbGUiOiJhZG1pbiJ9.yvHwc4Es0cF0ASi0HhHRYqs26O6rsMFYH7a0oXX_u5Y'
 }
 
