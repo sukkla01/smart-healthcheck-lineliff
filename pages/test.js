f31c882cba10244c57a0ee5034c3f92c7e103e5c
sukkla01 <sukkla01@gmail.com>
2023-01-19 16:44:34 +0700

    xxx

--- pages/test.js
+++ pages/test.js
@@ -1,27 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import * as googleTTS from 'google-tts-api';
 // const Audio = dynamic(import ('react-audioplayer'), { ssr: false })
+import axios from 'axios'
 
-
-const url = googleTTS.getAudioUrl('S002', {
-    lang: 'th',
-    slow: true,
-    host: 'https://translate.google.com',
-});
 
 const Test = () => {
     const [loading, setLoading] = useState(true);
-    const [audio] = useState(typeof Audio !== "undefined" && new Audio(url));
 
     useEffect(() => {
-        audio.play()
+        // audio.play()
     }, [])
 
 
+    const ontest = async () => {
+        let data = {
+            userid: 'U1b5792c2049b94a34abc87eedf946d2a'
+        }
+
+        try {
+            let res = await axios.post(`https://webhook-smart-healthcheck.diligentsoftinter.com/confirm`, data, { headers: { "Content-Type": 'application/json' } })
+
+        } catch (error) {
+            console.log(error)
+        }
+
+
+    }
 
     return (
         <div>
-
+            <button onClick={ontest}>ok</button>
         </div>
     )
 }
