f1775566aa0eec31f5018e591cd2f7e91bbadd33
sukkla01 <sukkla01@gmail.com>
2023-01-20 11:29:31 +0700

    x

--- pages/test3.js
+++ pages/test3.js
@@ -2,19 +2,23 @@ import { useState, useEffect } from "react"
 
 
 const Test3 = () => {
-    useEffect(() => {
-        // responsiveVoice.speak("hello world")
-        // window.responsiveVoice.speak("สวัสดี", "Thai Female");
-        // 
-        // var voicelist = responsiveVoice.getVoices();
-        // responsiveVoice.setDefaultVoice("Thai Female");
-        // responsiveVoice.enableWindowClickHook(true);
-        // responsiveVoice.setDefaultRate(1.0);
-        // responsiveVoice.speak("b001");
-        // console.log(voicelist)
-    }, [])
+  useEffect(() => {
+    // responsiveVoice.speak("hello world")
+    // window.responsiveVoice.speak("สวัสดี", "Thai Female");
+    // 
+    // var voicelist = responsiveVoice.getVoices();
+    // responsiveVoice.setDefaultVoice("Thai Female");
+    // responsiveVoice.enableWindowClickHook(true);
+    // responsiveVoice.setDefaultRate(1.0);
+    // responsiveVoice.speak("b001");
+    // console.log(voicelist)
+  }, [])
   return (
-    <div>test3</div>
+    <div style={{ fontFamily: 'Sarabun', marginTop: 10, width: '100%' }}   >
+      <div  style={{ backgroundColor: 'white', width: '100%', height: 750, borderColor: 'black' }}>
+        xxxxxx
+      </div>
+    </div>
   )
 }
 
